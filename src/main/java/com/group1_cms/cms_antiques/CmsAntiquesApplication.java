48c08e71eb2124f929a732ecea8a10b76e5c3aab
Luke <45085503+lukemunk@users.noreply.github.com>
2020-10-01 19:26:55 -0600

    initial

---
+++ src/main/java/com/group1_cms/cms_antiques/CmsAntiquesApplication.java
@@ -0,0 +1,13 @@
+package com.group1_cms.cms_antiques;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsAntiquesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsAntiquesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group1_cms/cms_antiques/CmsAntiquesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1_cms.cms_antiques;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmsAntiquesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
