dcf721d8c0198772fb883813e72295b32107c818
Austin Langston <mulock.shadowshade@gmail.com>
2020-10-20 16:31:33 -0600

    Started PostContentController
    Started coding for PostContentController
    To Pull info from Posts

---
+++ src/main/java/com/group1_cms/cms_antiques/controllers/PostContentController.java
@@ -0,0 +1,27 @@
+package com.group1_cms.cms_antiques.controllers;
+
+
+import com.group1_cms.cms_antiques.services.PostsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PostContentController
+{
+    private PostsService postsService;
+
+    @Autowired
+    public void PostsAndClassifiedsController(PostsService postsService)
+    {
+        this.postsService = postsService;
+    }
+
+    @RequestMapping(value="/posts/post")
+    public String getPostContent(Model model)
+    {
+        model.addAttribute("posts", postsService.getPosts());
+        return "public/postForum.html";
+    }
+}
