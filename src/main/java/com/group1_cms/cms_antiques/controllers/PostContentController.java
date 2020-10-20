d242926b9126fe883616b74b42ef4d0e846d23a5
Austin Langston <mulock.shadowshade@gmail.com>
2020-10-20 16:43:19 -0600

    Updated Post Rest API
    Switched to Rest API
    Added requests for content

--- src/main/java/com/group1_cms/cms_antiques/controllers/PostContentController.java
+++ src/main/java/com/group1_cms/cms_antiques/controllers/PostContentController.java
@@ -1,16 +1,24 @@
 package com.group1_cms.cms_antiques.controllers;
 
 
+import com.group1_cms.cms_antiques.models.Item;
+import com.group1_cms.cms_antiques.models.Post;
 import com.group1_cms.cms_antiques.services.PostsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/posts/post")
 public class PostContentController
 {
     private PostsService postsService;
+    private List<Post> currentPosts;
+    private Post currentPost;
 
     @Autowired
     public void PostsAndClassifiedsController(PostsService postsService)
@@ -18,10 +26,35 @@ public class PostContentController
         this.postsService = postsService;
     }
 
-    @RequestMapping(value="/posts/post")
-    public String getPostContent(Model model)
+    // Returns a post Page
+    @RequestMapping(value="/")
+    public String getPostPage(Model model)
     {
         model.addAttribute("posts", postsService.getPosts());
         return "postForum.html";
     }
+
+    //region Post Content
+    @GetMapping(path = "/",
+                produces = "application/json")
+    public String getTitle()
+    {
+        return currentPost.getTitle();
+    }
+
+    @GetMapping(path = "/",
+            produces = "application/json")
+    public String getStory()
+    {
+        return currentPost.getStory();
+    }
+
+    @GetMapping(path = "/",
+            produces = "application/json")
+    public Item getItem()
+    {
+        return currentPost.getItem();
+    }
+    //endregion
+
 }
