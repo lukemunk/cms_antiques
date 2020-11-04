0e46f486a57fe0fc9b58767e9b5110c84c9e3ed9
Luke <45085503+lukemunk@users.noreply.github.com>
2020-11-03 22:11:03 -0700

    added appImages/logoDesign, PasswordResetFormValidator, UserPasswordDto, accountProfile.html, closeAccount.html, fragments/header_footer_fragment.html, modifyCategory.html, modifyClassifieds.html, modifUserRoles, modifyUsers, modifyUserPermissions.html, resetPassword.html, css/headerStyles, css/homePageStyles,

--- src/main/java/com/group1_cms/cms_antiques/controllers/ViewController.java
+++ src/main/java/com/group1_cms/cms_antiques/controllers/ViewController.java
@@ -1,20 +1,27 @@
 package com.group1_cms.cms_antiques.controllers;
 
+import com.group1_cms.cms_antiques.components.PasswordResetFormValidator;
 import com.group1_cms.cms_antiques.components.RegistrationFormValidator;
 import com.group1_cms.cms_antiques.models.User;
+import com.group1_cms.cms_antiques.models.UserPasswordDto;
 import com.group1_cms.cms_antiques.services.PermissionService;
 import com.group1_cms.cms_antiques.services.RoleService;
 import com.group1_cms.cms_antiques.services.UserService;
 import com.group1_cms.cms_antiques.spring.security.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticatedPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class ViewController {
@@ -24,11 +31,14 @@ public class ViewController {
     private RoleService roleService;
     private PermissionService permissionService;
     private JwtTokenProvider jwtTokenProvider;
+    private PasswordResetFormValidator passwordResetFormValidator;
 
     @Autowired
-    public ViewController(RegistrationFormValidator registrationFormValidator, UserService userService,
-                          RoleService roleService, PermissionService permissionService, JwtTokenProvider jwtTokenProvider){
+    public ViewController(RegistrationFormValidator registrationFormValidator, PasswordResetFormValidator passwordResetFormValidator,
+                          UserService userService, RoleService roleService, PermissionService permissionService,
+                          JwtTokenProvider jwtTokenProvider){
         this.registrationFormValidator = registrationFormValidator;
+        this.passwordResetFormValidator = passwordResetFormValidator;
         this.userService = userService;
         this.roleService = roleService;
         this.permissionService = permissionService;
@@ -37,7 +47,8 @@ public class ViewController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public ModelAndView publicHomePage(){
-        return new ModelAndView("home");
+        ModelAndView modelAndView = new ModelAndView("home");
+        return modelAndView;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/login")
@@ -74,5 +85,39 @@ public class ViewController {
         jwtTokenProvider.autoLogin(newUser.getUsername(), newUser.getPasswordConfirm(), response);
 
         return new ModelAndView("redirect:/");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/member/accountProfile/{id}")
+    public ModelAndView editProfile(@PathVariable UUID id){
+        ModelAndView modelAndView = new ModelAndView("member/accountProfile");
+
+        return modelAndView;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/member/resetPassword")
+    public ModelAndView resetPassword(){
+        ModelAndView modelAndView = new ModelAndView("/member/resetPassword");
+        modelAndView.addObject("passwordResetForm", new UserPasswordDto());
+        return modelAndView;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/member/resetPassword")
+    public ModelAndView saveResetPassword(@ModelAttribute ("passwordResetForm") UserPasswordDto userPasswordDto, BindingResult bindingResult){
+        UserDetails currentUser = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        userPasswordDto.setUserName(currentUser.getUsername());
+        userPasswordDto.setEncryptedCurrentPassword(currentUser.getPassword());
+        passwordResetFormValidator.validate(userPasswordDto, bindingResult);
+
+        if(bindingResult.hasErrors()){
+            return new ModelAndView("member/resetPassword");
+        }
+        userService.resetUserPassword(userPasswordDto);
+        return new ModelAndView("redirect:/");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/member/closeAccount/{id}")
+    public ModelAndView closeAccount(@PathVariable UUID id){
+        ModelAndView modelAndView = new ModelAndView("member/closeAccount");
+        return modelAndView;
     }
 }
