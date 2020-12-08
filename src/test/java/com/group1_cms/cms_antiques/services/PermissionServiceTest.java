4dce29bf86ef8f56ca532d81504da00e835a48a5
Luke <45085503+lukemunk@users.noreply.github.com>
2020-12-08 13:41:23 -0700

    commented out unused Roleservice method.  Added tests for each RoleService method call.

---
+++ src/test/java/com/group1_cms/cms_antiques/services/PermissionServiceTest.java
@@ -0,0 +1,13 @@
+package com.group1_cms.cms_antiques.services;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(MockitoJUnitRunner.class)
+class PermissionServiceTest {
+}
